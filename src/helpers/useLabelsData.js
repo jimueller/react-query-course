0aa1d2005cac93a49f93ac9d1f643a54225150a3
Jim Mueller <jim@jimueller.com>
2022-07-24 12:03:00 -0500

    Done with 13 - Preloading Data

--- src/helpers/useLabelsData.js
+++ src/helpers/useLabelsData.js
@@ -1,9 +1,10 @@
 import { useQuery } from "react-query";
+import { defaultLabels } from "./defaultData";
 
 export function useLabelsData() {
   return useQuery(
     ["labels"],
     () => fetch("/api/labels").then((res) => res.json()),
-    { staleTime: 1000 * 60 * 60 }
+    { staleTime: 1000 * 60 * 60, placeholderData: defaultLabels }
   );
 }
