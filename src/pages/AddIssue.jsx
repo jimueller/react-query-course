5cc516708ffc54a55807cfb0baf79409d1db478b
Jim Mueller <jim@jimueller.com>
2022-08-20 16:11:32 -0500

    Done with 14 - Adding Issues

--- src/pages/AddIssue.jsx
+++ src/pages/AddIssue.jsx
@@ -1,3 +1,50 @@
+import { useMutation, useQueryClient } from "react-query";
+import { useNavigate } from "react-router-dom";
+
 export default function AddIssue() {
-  return <h2>Add Issue</h2>;
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const addIssue = useMutation(
+    (issue) => {
+      return fetch("/api/issues", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(issue),
+      }).then((res) => res.json());
+    },
+    {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(["issues"], { exact: true });
+        queryClient.setQueryData(["issues", data.number.toString()], data);
+        navigate(`/issue/${data.number}`);
+      },
+    }
+  );
+
+  return (
+    <div className="add-issue">
+      <h2>Add Issue</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (addIssue.isLoading) return;
+
+          addIssue.mutate({
+            title: event.target.title.value,
+            comment: event.target.comment.value,
+          });
+        }}
+      >
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name="title" placeholder="Title" />
+        <label htmlFor="comment">Comment</label>
+        <textarea name="comment" id="comment" rows="5" />
+        <button type="submit" disabled={addIssue.isLoading}>
+          {addIssue.isLoading ? "Adding..." : "Add Issue"}
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
 import { StatusSelect } from "../components/StatusSelect";
+import { Link } from "react-router-dom";
 
 export default function Issues() {
   const [labels, setLabels] = useState([]);
@@ -31,6 +32,9 @@ export default function Issues() {
             value={status}
             onChange={(event) => setStatus(event.target.value)}
           ></StatusSelect>
+          <Link className="button" to="/add">
+            Add Issue
+          </Link>
         </aside>
       </main>
     </div>
